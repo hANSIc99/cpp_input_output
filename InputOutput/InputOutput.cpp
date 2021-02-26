dc2f3e11bfa9e46e9f404468f40020d4cde0f8ca
Stephan Avenwedde <StephanA@beckhoff.com>
2021-02-26 08:18:53 +0100

    program ready

--- InputOutput/InputOutput.cpp
+++ InputOutput/InputOutput.cpp
@@ -1,20 +1,51 @@
-// InputOutput.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
+#include <iostream> // cout, cin, cerr etc...
+#include <fstream> // ifstream, ofstream
+#include <string>
 
-#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string sFilename = "MyFile.txt";    
+
+    /******************************************
+     *                                        *
+     *                WRITING                 *
+     *                                        *
+     ******************************************/
+
+    std::ofstream fileSink(sFilename); // Creates an output file stream
+
+    if (!fileSink) {
+        std::cerr << "Canot open " << sFilename << std::endl;
+        exit(-1);
+    }
+
+    /* std::endl will automatically append the correct EOL */
+    fileSink << "Hello Open Source World!" << std::endl;
+
+    /******************************************
+     *                                        *
+     *                READING                 *
+     *                                        *
+     ******************************************/
+    
+    std::ifstream fileSource(sFilename); // Creates an input file stream
+
+    if (!fileSource) {
+        std::cerr << "Canot open " << sFilename << std::endl;
+        exit(-1);
+    }
+    else {
+        // Intermediate buffer
+        std::string buffer;
+
+        // By default, the >> operator reads word by workd (till whitespace)
+        while (fileSource >> buffer) 
+        {
+            std::cout << buffer << std::endl;
+        }
+    }
+
+    exit(0);
 }
 
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
